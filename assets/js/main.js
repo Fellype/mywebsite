49be89fdf736679806f585c6dbd2947e8a560105
Fellype <fellypenc@gmail.com>
2017-08-23 01:05:14 -0300

    translations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,136 @@
-
-
-                    
+$('#selectLanguageDropdown').localizationTool({
+    'defaultLanguage' : 'pt_BR', /* (optional) although must be defined if you don't want en_GB */
+    'showFlag': false,            /* (optional) show/hide the flag */
+    'showCountry': false,         /* (optional) show/hide the country name */
+    'showLanguage': true,        /* (optional) show/hide the country language */
+    /* 
+     * Translate your strings below
+     */
+    'strings' : {
+        /* 
+         * You can specify the text string to translate directly... 
+         */
+        'class:welcome-lang' : {
+            'en_GB' : 'Hi, I work for about 7 years with web interface development and over time I have been learning that technologies should be more than beautiful, they should approach what is intuitive, easy and functional for humans. On this journey, nothing was better for my learning than the human contact, through UX testing and user-focused development, as well as using intelligent CSS, always planning the behavior of the visual components.',
+            'es_ES' : 'Hola, trabajo hace unos 7 años con el desarrollo de interfaces para la web y con el tiempo he aprendido que las tecnologías deben ser más que bonitas, que deben acercarse a lo que es intuitivo, fácil y funcional a los humanos. En esta jornada, nada fue mejor para mi aprendizaje que el propio contacto con los humanos, a través de pruebas de UX y desarrollo enfocado en el usuario, además de la utilización de CSS inteligente, planeando siempre el comportamiento de los componentes visuales.'
+        },
+        'Contato' : {
+            'en_GB' : 'Contact',
+            'es_ES' : 'Contacto'
+        },
+        'Testes de usabilidade' : {
+            'en_GB' : 'Usability tests',
+            'es_ES' : 'Pruebas de usabilidad'
+        },
+        'class:uxweekend' : {
+            'en_GB' : 'Workshop about UX Design, with a workload of 14 hours, held on August 16 and 17, 2014 at 4Legal, in Brasília, Distrito Federal.',
+            'es_ES' : 'Taller de Diseño UX, con un volumen de trabajo de 14 horas, que se celebró el 16 y 17 de agosto de 2014 en 4Legal, Brasilia, Distrito Federal.'
+        },
+        'class:canvas' : {
+            'en_GB' : 'Workshop about strategies for validation of new business with a 16-hour workload, held on December 7 and 14, 2013 in CASULO - Business Incubators of the UniCEUB, Brasília, Distrito Federal.',
+            'es_ES' : 'Taller sobre estrategias para validación de nuevos negocios con carga horaria de 16 horas, realizado en los días 7 y 14 de diciembre de 2013 en CASULO - Incubadoras de Empresas del UniCEUB, Brasília, Distrito Federal.'
+        },
+        'class:exp-om' : {
+            'en_GB' : 'One of the biggest projects of my career, in ReclameAQUI I worked from the initial conceptions of their new product, OMediador, until the final implementation of the screens and flows of the system. In a step-by-step approach, I did many visual concepts sometimes in prototype forms for selling the product to investors. As responsible for the flow and usability of the system, I guided some front-end developers into the task of choosing the best alternatives for component creation and visual implementations. It was there, also, that I started using css architecture for organization and improvement of maintainability.',
+            'es_ES' : 'Uno de los mayores proyectos de mi carrera, en el ReclameAQUI, he trabajado desde las concepciones iniciales del nuevo producto de ellos, OMediador, hasta la implementación final de las pantallas y flujos del sistema. En un enfoque gradual, he hecho muchos conceptos visuales a veces en formas de prototipos para la venta del producto a los inversores. Como responsable de los flujos y la usabilidad del sistema, he guiado a algunos desarrolladores front-end en la tarea de elegir las mejores alternativas para la creación de componentes e implementaciones visuales. Fue ahi que empecé a utilizar arquitectura de css para organización y mejora de la manutención.'
+        },
+        'class:exp-lab' : {
+            'en_GB' : 'In Labhacker we developed technological innovations whose principles were to increase political transparency and popular participation in the legislative process. Throughout my experience I helped create a variety of engagement tools, where it was part of the process, user testing. The strength of our work, given that there were frequent visits of people of all types and ages in the Chamber, and especially in the labhacker.',
+            'es_ES' : 'En el labhacker desarrollamos innovaciones tecnológicas cuyos principios eran aumentar la transparencia política y la participación popular en el proceso legislativo. A lo largo de mi experiencia he ayudado a crear diversas herramientas de participación, donde era parte del proceso, las pruebas con los usuarios. Punto fuerte de nuestro trabajo, hay vista que había en la Cámara, y en especial en el labhacker, visitas frecuentes de personas de todo tipo y edades.'
+        },
+        'class:exp-pmp' : {
+            'en_GB' : "Startup where I started to work more focused on mobile. I worked on developing responsive web interfaces and helped plan and build wireframes and interfaces for the company's core app.",
+            'es_ES' : 'Startup donde empecé a trabajar más enfocado en mobile. He trabajado con el desarrollo de interfaces responsivas para la Web y he ayudado en la planificación y construcción de wireframes y interfaces para la aplicación principal de la empresa.'
+        },
+        'class:exp-vel' : {
+            'en_GB' : 'Experience as Graphic Designer and Web Designer in this digital marketing company. Performing several typical agency jobs and also developing some websites. It was my first experiences with Wordpress.',
+            'es_ES' : 'Experiencia como Diseñador gráfico y Web Designer en esta empresa de marketing digital. Realizando varios trabajos típicos de agencia y también desarrollando algunos sitios. Fue mi primera experiencia con Wordpress.'
+        },
+        'class:exp-cdt' : {
+            'en_GB' : 'Internship at this company, which is a Startup incubator at the University of Brasilia. My first professional contacts with web development and other experiences with augmented reality.',
+            'es_ES' : 'Prácticas en la incubadora de Startups de la Universidad de Brasilia. Mis primeros contactos profesionales con desarrollo web y otras experiencias con realidad aumentada.'
+        },
+        'Laboratório Hacker da Câmara dos Deputados' : {
+            'en_GB' : 'Hacker Lab of the Chamber of Deputies',
+            'es_ES' : 'Laboratório Hacker de la Cámara de Diputados'
+        },
+        'Centro de Apoio ao Desenvolvimento Tecnológico' : {
+            'en_GB' : 'Technological Development Support Center',
+            'es_ES' : 'Centro de Apoyo al Desarrollo Tecnológico'
+        },
+        'Outras experiências' : {
+            'en_GB' : 'Other experiences',
+            'es_ES' : 'Otras experiencias'
+        },
+        'Professor de webdesign' : {
+            'en_GB' : 'Webdesign Teacher',
+            'es_ES' : 'Profesor de webdesign'
+        },
+        'Projeto de Pesquisa (PRODC) em desenvolvimento de jogos educativos para PC' : {
+            'en_GB' : 'Research Project (PRODC) in the development of educational games for PC',
+            'es_ES' : 'Proyecto de Investigación (PRODC) en desarrollo de juegos educativos para PC'
+        },
+        'Projeto de Pesquisa (PRODC) em desenvolvimento de interface web para sistema de carona solidária' : {
+            'en_GB' : 'Research Project (PRODC) in development of web interface for carpooling system',
+            'es_ES' : 'Proyecto de Investigación (PRODC) en desarrollo de interfaz web para sistema de carona solidaria'
+        },
+        'Projeto com visualização de dados e data-driven utilizando d3.js' : {
+            'en_GB' : 'Project with data visualization and data-driven using d3.js',
+            'es_ES' : 'Proyecto con visualización de datos y data-driven utilizando d3.js'
+        },
+        'Educação' : {
+            'en_GB' : 'Education',
+            'es_ES' : 'Educación'
+        },
+        'Técnico em informática' : {
+            'en_GB' : 'COMPUTER TECHNICIAN',
+            'es_ES' : 'TÉCNICO EN INFORMÁTICA'
+        },
+        'class:ed-tec' : {
+            'en_GB' : 'High school with integrated technical course. First contacts with webdesign.',
+            'es_ES' : 'Secundaria con curso técnico integrado. Primeros contactos con el diseño web.'
+        },
+        'Engenharia de Software' : {
+            'en_GB' : 'SOFTWARE ENGINEERING',
+            'es_ES' : 'INGENIERÍA DE SOFTWARE'
+        },
+        'class:ed-eng' : {
+            'en_GB' : 'I did a few semesters of software engineering, learned lots of things, from advanced mathematical concepts of engineering, to methodologies and software development processes. Great period for experiments where I was a researcher.',
+            'es_ES' : 'Hice algunos semestres de ingeniería de software, aprendí bastante, desde conceptos matemáticos avanzados de la ingeniería, hasta metodologías y procesos de desarrollo de software. Período óptimo para experiencias donde fui becario de proyectos de investigación.'
+        },
+        'Computação' : {
+            'en_GB' : 'COMPUTING',
+            'es_ES' : 'COMPUTACIÓN'
+        },
+        'class:ed-com' : {
+            'en_GB' : 'Experience enriching with the degree course, mainly in the disciplines of psychology where I improved my knowledge in User Experience.',
+            'es_ES' : 'Experiencia enriquecedora con el curso de licenciatura, principalmente por cuenta de las disciplinas de psicología donde perfeccioné mis conocimientos en Experiencia de Usuario.'
+        },
+        'Inglês' : {
+            'en_GB' : 'English',
+            'es_ES' : 'Inglés'
+        },
+        'Espanhol' : {
+            'en_GB' : 'Spanish',
+            'es_ES' : 'Español'
+        },
+        'incompleto' : {
+            'en_GB' : 'incomplete',
+            'es_ES' : 'incompleto'
+        },
+        'Conheça alguns dos meus trabalhos' : {
+            'en_GB' : 'Get to know some of my works',
+            'es_ES' : 'Conozca algunos de mis trabajos'
+        },
+        'experiências' : {
+            'en_GB' : 'Experiences',
+            'es_ES' : 'Experiencias'
+        },
+        'galeria' : {
+            'en_GB' : 'Gallery',
+            'es_ES' : 'Galería'
+        },
+        
+    }
+});
+// end of step-by-step guide
